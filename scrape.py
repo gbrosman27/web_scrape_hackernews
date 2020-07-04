54df3a4abdb583a34dbfedf03c028bed2b130116
Greg Brosman <greg.brosman@gmail.com>
2020-07-04 13:34:38 -0700

    import modules and get request

---
+++ scrape.py
@@ -0,0 +1,15 @@
+# https://news.ycombinator.com/robots.txt
+# Crawl-delay: 30 seconds
+
+
+import requests
+from bs4 import BeautifulSoup
+
+
+response = requests.get("https://news.ycombinator.com/news")
+
+
+
+
+
+
