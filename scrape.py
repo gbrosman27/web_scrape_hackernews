cbc5c3aabee571c8fd48516612b9cd474970a318
Greg Brosman <greg.brosman@gmail.com>
2020-07-04 14:34:05 -0700

    create_custom method

--- scrape.py
+++ scrape.py
@@ -4,12 +4,34 @@
 
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
-
+# Get the website save the parsed html.
 response = requests.get("https://news.ycombinator.com/news")
+parsed_html = BeautifulSoup(response.text, 'html.parser')
+
+# Use CSS selector to grab data.
+article_links = parsed_html.select('.storylink')
+art_subtext = parsed_html.select('.subtext')
 
 
+def create_custom(links, subtext):
+    """Creates a list of article names and their links."""
+    news_list = []
+    for index, item in enumerate(links):
+        title = links[index].getText()
+        href = links[index].get('href', None)
+        subtext_votes = subtext[index].select('.score')
+        # If article has votes, convert to int, drop "points" suffix.
+        if len(subtext_votes):
+            votes = int(subtext_votes[0].getText().replace(' points', ''))
+            # If votes are > 99, append to list.
+            if votes > 99:
+                news_list.append({'title': title, 'link': href, 'votes': votes})
+    return news_list
 
+
+pprint.pprint(create_custom(article_links, art_subtext))
 
 
 
